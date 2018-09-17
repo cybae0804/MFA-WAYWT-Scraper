5bac2e174dc0fcd1b7549090bfb88743e88a36de
cybae0804 <cybae0804@gmail.com>
2018-09-16 20:48:08 -0700

    v2

--- scraper.py
+++ scraper.py
@@ -23,57 +23,62 @@ for brand in brandNames:
 kwtree.finalize()
 
 # Initializing praw reddit client
-reddit = praw.Reddit(client_id=input('client_id: '),
-	client_secret=input('client_secret: '),
-	password=input('password: '),
+client_id = input('client_id: ')
+client_secret = input('client_secret: ')
+username = input('username: ')
+password = input('password: ')
+reddit = praw.Reddit(client_id=client_id,
+	client_secret=client_secret,
+	password=password,
 	user_agent='WAYWT Stats Scraper',
-	username=input('username: '))
+	username=username)
 
-# praw search return
+# praw search
 totalList = []
+time_filter = input('time filter: ')
 for submission in reddit.subreddit('malefashionadvice').search(
-	'title:WAYWT AND NOT Top AND NOT Summer AND NOT Theme AND NOT ANNOUNCEMENT AND NOT META AND NOT Monthlong', 
-	sort='new', time_filter='month'):
+	'title:WAYWT NOT title:Top NOT title:Summer NOT title:Fit NOT title:Theme NOT title:Edition NOT title:META NOT title:Monthlong', 
+	sort='new', time_filter=time_filter):
 	print(submission.title)
 	indivList = []
 	for top_level_comment in submission.comments:
+		brandCount = 0
+		repeatSet = set()
+
+		# if the post had been deleted, it won't store the author name. Later, rows w/o author names get dropped.
 		if (top_level_comment.author):
 			indivList.append(top_level_comment.author.name)
-			print(top_level_comment.author.name)
-			print(top_level_comment.body)
 
-		commentDate = time.strptime(time.ctime(top_level_comment.created_utc))
-		indivList.append(commentDate.tm_year)
-		indivList.append(commentDate.tm_mon)
-		indivList.append(commentDate.tm_mday)
-		indivList.append(top_level_comment.score)
-		results = kwtree.search_all(top_level_comment.body)
-		brandCount = 0
+			commentDate = time.strptime(time.ctime(top_level_comment.created_utc))
+			indivList.append(commentDate.tm_year)
+			indivList.append(commentDate.tm_mon)
+			indivList.append(commentDate.tm_mday)
+			indivList.append(top_level_comment.score)
+			results = kwtree.search_all(top_level_comment.body)
 
-		repeatSet = set()
-		for result in results:
-			if result[0] in repeatSet:
-				print('REPEAT FOUND')
-			else:
-				repeatSet.add(result[0])
-				indivList.append(result[0])
-				brandCount += 1
-				print(result)
-			#print("Current set: ", repeatSet)
-			#print("Current input: ", result[0])
+			# For my analysis, an individual wearing more than one item of the same brand is not repeated for that post.
+			for result in results:
+				if result[0] not in repeatSet:
+					repeatSet.add(result[0])
+					indivList.append(result[0])
+					brandCount += 1
+
 		while brandCount < 7:
 			indivList.append('0')
 			brandCount += 1
-		#print(indivList)
+
 		indivList.append(top_level_comment.permalink)
+
 		if (len(indivList) == 13):
-			totalList.append(indivList.copy())
-			#print('ok')
+			totalList.append(list(indivList))
+
 		indivList.clear()
+
 df = pd.DataFrame(totalList, columns=['username', 'year', 'month', 'day', 'score', 'BN1', 'BN2', 'BN3', 'BN4', 'BN5', 'BN6', 'BN7', 'permalink'])
 
+# shortened brand names restored to official names
 df = df.replace('BR ', 'Banana Republic')
-df = df.replace('Birk ', 'Birkenstocks')
+df = df.replace('Birk', 'Birkenstocks')
 df = df.replace('OL ', 'Our Legacy')
 df = df.replace('Yohji', 'Yohji Yamamoto')
 df = df.replace(['J.Crew', 'J. Crew', 'J crew', 'jcrew'], 'J. Crew')
@@ -100,13 +105,11 @@ df = df.replace('Onitsuka', 'Onitsuka Tiger')
 df = df.replace('Levi ', 'Levis')
 df = df.replace('Comme des', 'Comme des Garcons')
 df = df.replace('SEF', 'Story Et Fall')
-df = df.replace('Martens ', 'Doc Martens')
+df = df.replace('Brooks', 'Brooks Brothers')
+df = df.replace('Martens', 'Doc Martens')
 df = df.replace('Gitman', 'Gitman Vintage')
 df = df.replace('Hilfiger', 'Tommy Hilfiger')
 df = df.replace('CP ', 'Common Projects')
 
-
-
-
-
+# exports the dataframe to a csv file for analysis
 df.to_csv('cleanData.csv')
